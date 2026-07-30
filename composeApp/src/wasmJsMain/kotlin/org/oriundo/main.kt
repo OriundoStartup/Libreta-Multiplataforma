f8735f982447c972af1b723d41ff79b4db2f29b7
OriundoStartup <juan.camposgomez80@gmail.com>
2026-07-29 22:41:42 -0400

    chore: force new deployment to verify sync and login fixes

--- composeApp/src/wasmJsMain/kotlin/org/oriundo/main.kt
+++ composeApp/src/wasmJsMain/kotlin/org/oriundo/main.kt
@@ -19,6 +19,7 @@ fun main() {
     val loadingScreen = document.getElementById("loading-screen") as? HTMLElement
     
     println("Web App: Starting Boot Sequence...")
+    println("Web App: Forcing deployment sync - July 29 2026")
     
     try {
         // 1. Validación de Configuración
